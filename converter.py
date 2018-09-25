a14fad20c23032553820e3749a42d3542b65ba0e
speed4andy <speed4andy@gmail.com>
2018-09-25 15:27:24 +0200

    converter

--- converter.py
+++ converter.py
@@ -2,14 +2,20 @@
 
 import sys
 
-q1 = input("Co chces prevest? decimal/binar? popripade jine?: ")
+#q1 = input("Co chces prevest? decimal/binar? popripade jine?: ")
+q3 = int(input("Nabizim prevod z desitkove(10) do osmickove(8) a sestnactkove(16), napis patricne cislo k dane soustave : "))
 q2 = int(input("Zadej cislo k prevodu: "))
-q3 = input("Nabizim prevod z desitkove do osmickove a sestnactkove, napis patricne cislo k dane soustave: ")
 
-print(q1)
-if q1 == "decimal":
+print(q3)
+
+if q3 == 10:
     print(q2)
-    print("Zadane cislo z desitkove do dvojkove soustavy je: " + bin(q2))     
+    print("Zadane cislo z desitkove do dvojkove soustavy je: " + bin(q2))
+elif q3 == 8:
+    print(q2)
+    print("Zadane cislo z desitkove do osmickove soustavy je: " + oct(q2))
+elif q3 == 16:
+    print(q2)
+    print("Zadane cislo z desitkove do sestnactkove soustavy je: " + hex(q2))
 else:
-    if q1 == "jine":
-             
+    print("Toto je spatna volba")  
